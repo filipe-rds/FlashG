52d2c235dc5d07aa63af87fcba9ffe4bf995990a
Gabriel Félix <111694250+gfedacs@users.noreply.github.com>
2024-10-29 19:08:45 +0000

    [FG-5]-feature: Criação do modelo Fotográfo.
    --- Modificações:
    
    -- Foi criado a classe Fotográfo na pasta model.

--- src/main/java/br/edu/ifpb/pweb2/flashg/model/Photographer.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/model/Photographer.java
@@ -1,4 +1,21 @@
 package br.edu.ifpb.pweb2.flashg.model;
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+
+@Entity
 public class Photographer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id
+    private string name;
+    @Column(unique = true)
+    private string email;
+
 }
+
+
