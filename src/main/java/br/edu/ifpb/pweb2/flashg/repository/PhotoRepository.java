94f554de3d8207ec712228599aec51617a94f4d3
Johnner Yelcde <johnnercastrojy@gmail.com>
2024-10-30 14:46:58 -0230

    [FG-10] ✨feat: repositório de Photo

--- src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotoRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotoRepository.java
@@ -1,4 +1,11 @@
 package br.edu.ifpb.pweb2.flashg.repository;
 
-public interface PhotoRepository {
+
+import br.edu.ifpb.pweb2.flashg.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    public Photo findbyId(Integer id);
 }
