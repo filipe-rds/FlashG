c125ef6aa5609edb9e6dff10b66e22f0249b7b73
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-10-30 02:00:32 -0300

    [FG-9] ✨ feat: cria modelo para Photo

--- src/main/java/br/edu/ifpb/pweb2/flashg/model/Photo.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/model/Photo.java
@@ -1,4 +1,22 @@
 package br.edu.ifpb.pweb2.flashg.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    private Photographer photographer;
+
+    private String imageUrl;
+    private byte[] imageData;
 }
