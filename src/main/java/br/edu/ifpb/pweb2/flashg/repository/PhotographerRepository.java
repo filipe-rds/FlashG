13526a2c1516f1d3899c7ddf82a1f75b1ce5e2b1
Filipe Rodrigues <73905894+filipe-rds@users.noreply.github.com>
2024-10-30 17:59:23 +0000

    [FG-6] ✨ feat: criado nova função no repositório
    --- Modificações:
    
    - Foi criado a função FindByName com o intuito de retornar fotógrafos que possuem o mesmo nome.

--- src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotographerRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotographerRepository.java
@@ -4,9 +4,13 @@ package br.edu.ifpb.pweb2.flashg.repository;
 import br.edu.ifpb.pweb2.flashg.model.Photographer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface PhotographerRepository extends JpaRepository<Photographer, Integer> {
-    public Photographer findByEmail(String email);
+    Photographer findByEmail(String email);
+
+    List<Photographer> findByName (String name);
 
 }
