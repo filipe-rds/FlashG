5b4210741b6277691bc7bc418913b5bfbfd5cc3d
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2024-10-30 09:11:52 -0300

    [FG-13] ♻️ refactor: adicionando anotações lombok
    --- Modificações:
    
    - Foi adicionado anotações pendentes da classe Photographer.

--- src/main/java/br/edu/ifpb/pweb2/flashg/model/Photographer.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/model/Photographer.java
@@ -1,20 +1,23 @@
 package br.edu.ifpb.pweb2.flashg.model;
+
 import jakarta.persistence.*;
-import lombok.Setter;
-import lombok.Getter;
-import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Photographer {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id
-    private string name;
+    private int id;
+
     @Column(unique = true)
-    private string email;
+    private String email;
+
+    private String name;
 
 }
 
