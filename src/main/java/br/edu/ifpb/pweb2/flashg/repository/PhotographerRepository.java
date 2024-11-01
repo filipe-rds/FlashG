a2481885a3e6300e8fe88f43a89a1a4c1dd05066
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2024-11-01 09:53:48 -0300

    [FG-19] ♻️ refactor: otimizando consulta findByEmail
    --- Modificações:
    
    - Foi adicionado a classe utilitária Optional para evitar o estouro de exceção NullPointerException e consequentemente otimizar seu tratamento.

--- src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotographerRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/repository/PhotographerRepository.java
@@ -5,11 +5,13 @@ import br.edu.ifpb.pweb2.flashg.model.Photographer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface PhotographerRepository extends JpaRepository<Photographer, Integer> {
-    Photographer findByEmail(String email);
+
+    Optional<Photographer> findByEmail(String email);
 
     List<Photographer> findByName (String name);
 
