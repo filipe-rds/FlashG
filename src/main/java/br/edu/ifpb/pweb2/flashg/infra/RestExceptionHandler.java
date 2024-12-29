99242ed9e6f9f95c5fa532f4a044d151053ba4b2
Gabriel Félix <gfedacs@hotmail.com>
2024-12-29 01:45:29 -0300

    [FG-82]-feat: Criação dos diretórios e exemplos.
    ---- Modificações:
    
    --- Foi criado uma pasta chamada exception que vai armazenar todas as classes de erro na aplicação.
    
    --- Foi criado outra pasta chamada de infra, que vai armazenar a classe RestExcepetionHandler, que será responsável por tratar todas as exceções do sistema.

---
+++ src/main/java/br/edu/ifpb/pweb2/flashg/infra/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package br.edu.ifpb.pweb2.flashg.infra;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.edu.ifpb.pweb2.flashg.exceptioon.NotFoundPhotoException;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
+
+    @ExceptionHandler(NotFoundPhotoException.class)
+    private ModelAndView handleNotFoundPhotoException( NotFoundPhotoException ex){
+
+        return new ModelAndView("error/404");
+
+    }
+    
+}
+
+
+// Aqui eh responsável por tratar todos os erros que ocorrem na aplicação. Como a gente ta usando View,
+// vamos retornar um ModelAndView com a página de erro 404. Se fosse uma API, retornaria um ResponseEntity.
+// Se for um erro crítico, a gente pode direncionar pra uma pagina de erro default, mas se for um erro específico,
+// a gente pode tratar de outra forma, como por exemplo, mostrar uma mensagem no canto inferior, dizendo que não existe
+// a foto que o usuário está tentando acessar.
