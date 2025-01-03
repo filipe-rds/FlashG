3d8f47ea813f536f0be28e693872ab191140b724
Gabriel Félix <gfedacs@hotmail.com>
2025-01-03 00:06:25 -0300

    [FG-84]-feat: Criação de controlador teste.
    ---- Modificações:
    
    --- Foi criado um controlador de teste para o layout.

---
+++ src/main/java/br/edu/ifpb/pweb2/flashg/controller/TestandoController.java
@@ -0,0 +1,20 @@
+package br.edu.ifpb.pweb2.flashg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+@RequestMapping("/testando")
+public class TestandoController {
+
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
+    public ModelAndView save(ModelAndView mav) {
+        mav.setViewName("index");
+        return mav;
+    }
+}
+
