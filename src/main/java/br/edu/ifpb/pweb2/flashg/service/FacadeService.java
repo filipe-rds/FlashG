c6b5108360884b432d508b2e6c65c7408190d2e2
Lucas Pedro <lucasjaud19@gmail.com>
2025-01-07 23:02:16 -0300

    [FG-8]Feat: Criado Servic de Photo e adicionado funcionalidades na fachada
    ---Modificações:
    
    ---feat:Adiconado função para receber as  fotos e armazenar no banco

--- src/main/java/br/edu/ifpb/pweb2/flashg/service/FacadeService.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/service/FacadeService.java
@@ -1,14 +1,18 @@
 package br.edu.ifpb.pweb2.flashg.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import br.edu.ifpb.pweb2.flashg.entity.Photo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifpb.pweb2.flashg.entity.Follow;
 import br.edu.ifpb.pweb2.flashg.entity.FollowId;
 import br.edu.ifpb.pweb2.flashg.entity.Photographer;
+import br.edu.ifpb.pweb2.flashg.entity.Photo;
 import br.edu.ifpb.pweb2.flashg.repository.PhotographerRepository;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Service
@@ -16,6 +20,9 @@ public class FacadeService {
 
     @Autowired
     private PhotographerService photographerService;
+
+    @Autowired
+    private PhotoService photoService;
 
     @Autowired
     private PhotographerRepository photographerRepository;
@@ -82,5 +89,20 @@ public class FacadeService {
     public List<Photographer> findAllFollowing(Long id){
         return photographerService.findAllFollowing(id);
     }
-    
+
+
+    public void uploadPhoto(Long id, MultipartFile file) throws IOException {
+        Photographer photographer = photographerService.findById(id);
+        Photo photo = new Photo();
+        photo.setPhotographer(photographer);
+        //chama metodo que vai inserir a foto no banco
+        photoService.create(photo,file);
+        //adiciona a foto a lista de fotos do fotografo
+        List<Photo> lista = photographer.getPhotos();
+        lista.add(photo);
+        photographer.setPhotos(lista);
+        photographerRepository.save(photographer);
+
+    }
+
 }
