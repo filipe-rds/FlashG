2e5d3c583506abfc9e4a0b0a4fb08c2a2004f082
Gabriel Félix <gfedacs@hotmail.com>
2025-01-09 12:41:10 -0300

    [FG-29]-feat: Criação de métodos no FacadeController.
    ---- Modificações:
    
    --- Implementados os métodos followAction, e refatorados os métodos showProfile e listAllFollowing.

--- src/main/java/br/edu/ifpb/pweb2/flashg/controller/FacadeController.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/controller/FacadeController.java
@@ -1,21 +1,20 @@
 package br.edu.ifpb.pweb2.flashg.controller;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
-import br.edu.ifpb.pweb2.flashg.entity.Photo;
-import br.edu.ifpb.pweb2.flashg.service.PhotographerService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.edu.ifpb.pweb2.flashg.entity.Follow;
 import br.edu.ifpb.pweb2.flashg.entity.Photographer;
 import br.edu.ifpb.pweb2.flashg.service.FacadeService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/facade")
@@ -30,10 +29,12 @@ public class FacadeController {
         return mav;
     }
 
-
+    
     @RequestMapping(value = "/findPhotographers", method = RequestMethod.GET)
-    public ModelAndView getAllPhotographerStartingWith(ModelAndView mav, @RequestParam("username") String username){
+    public ModelAndView getAllPhotographerStartingWith(ModelAndView mav, @RequestParam("username") String username, HttpSession session){
         List<Photographer> Photographers = facadeService.findByUsernameStartingWith(username);
+        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
+        facadeService.removePhotographerFromArray(Photographers,loggedPhotographer);
         mav.setViewName("application/findPhotographers");
         mav.addObject("photographers", Photographers);
         mav.addObject("resultados", "Resultados para: "+ username); 
@@ -41,10 +42,13 @@ public class FacadeController {
     }
 
     @RequestMapping(value = "/showProfile/{id}", method = RequestMethod.GET)
-    public ModelAndView showProfile(ModelAndView mav, @PathVariable("id") Long id){
+    public ModelAndView showProfile(ModelAndView mav, @PathVariable("id") Long id,  HttpSession session){
         mav.setViewName("application/showProfilePhotographer");
-        Photographer photographer = facadeService.findByIdPhotographer(id);
-        mav.addObject("photographer", photographer);
+        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
+        Photographer seguido = facadeService.findByIdPhotographer(id);
+        String status  = facadeService.checkFollowStatus(loggedPhotographer, seguido);
+        mav.addObject("status", status);
+        mav.addObject("photographer", seguido);
         return mav;
     }
 
@@ -56,50 +60,26 @@ public class FacadeController {
     //     return mav;
     // }
 
-    @RequestMapping(value = "/follow/{id}/{id2}", method = RequestMethod.POST)
-    public ModelAndView follow (ModelAndView mav , @PathVariable("id") Long id, @PathVariable("id2") Long id2){
-        facadeService.follow(id, id2);
-        mav.setViewName("application/listPhotographerFollowing");
-        List<Photographer> photographes = facadeService.findAll();
-        mav.addObject("photographers", photographes);
+    @RequestMapping(value = "/followAction", method = RequestMethod.POST)
+    public ModelAndView followAction (ModelAndView mav , @RequestParam("id") Long id,HttpSession session){
+        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
+        facadeService.handleFollowAction(loggedPhotographer.getId(), id);
+        // Atualiza o objeto do fotografo logado
+        Photographer updatedloggedPhotographer = facadeService.findByIdPhotographer(loggedPhotographer.getId());
+        // Atualiza a sessão com o novo status
+        session.setAttribute("loggedPhotographer", updatedloggedPhotographer);
+        mav.setViewName("redirect:/facade/showProfile/"+id);
+        //redirectAttributes.addFlashAttribute("status", "Deix");
+        
         return mav;
     }
 
-    @RequestMapping(value = "/listPhotographerFollowing/{id}", method = RequestMethod.GET)
-    public ModelAndView listAllFollowing(ModelAndView mav, @PathVariable("id") Long id ){ 
-        List<Photographer> seguidos = facadeService.findAllFollowing(id);
+    @RequestMapping(value = "/listPhotographerFollowing", method = RequestMethod.GET)
+    public ModelAndView listAllFollowing(ModelAndView mav, HttpSession session){ 
+        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
+        List<Photographer> seguidos = facadeService.findAllFollowing(loggedPhotographer.getId());
         mav.setViewName("application/listPhotographerFollowing");
         mav.addObject("seguidos", seguidos);
         return mav;
     }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.GET)
-    public ModelAndView uploadPage(ModelAndView mav) {
-        mav.addObject("photo", new Photo());
-        mav.setViewName("application/uploadPhotos");
-        return mav;
-    }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public ModelAndView handleFileUpload(ModelAndView mav, HttpSession session, @RequestParam("image") MultipartFile file, @ModelAttribute("photo")Photo photo) throws Exception {
-        if (file.isEmpty()) {
-            mav.setViewName("application/uploadPhotos");
-            return mav;
-        }
-        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
-        facadeService.uploadPhoto(loggedPhotographer.getId(),photo,file);
-        mav.setViewName("application/uploadPhotos");
-        return mav;
-    }
-
-    @RequestMapping(value = "/myPhotos", method = RequestMethod.GET)
-    public ModelAndView showPhotographerPhotos(ModelAndView mav,HttpSession session) {
-        Photographer loggedPhotographer = (Photographer) session.getAttribute("loggedPhotographer");
-        List<Photo> photos = facadeService.showPhotos(loggedPhotographer.getId());
-        mav.setViewName("application/myPhotos");
-        mav.addObject("photographer", loggedPhotographer);
-        mav.addObject("photos", photos);
-        return mav;
-    }
-
 }
