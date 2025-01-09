bd700c176b7a740c8544129964f8eee8acced90a
Gabriel Félix <gfedacs@hotmail.com>
2025-01-09 16:42:04 -0300

    [FG-29]-feat: Adição de condição
    ---- Modificações:
    
    --- Agora o atributo username será único.

--- src/main/java/br/edu/ifpb/pweb2/flashg/entity/Photographer.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/entity/Photographer.java
@@ -17,7 +17,7 @@ public class Photographer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
 
     @Column(nullable = false)
