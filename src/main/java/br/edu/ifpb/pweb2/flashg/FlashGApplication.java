f01c8f21047834e803d99875b98dbb81e0ea5f5c
Gabriel Félix <gfedacs@hotmail.com>
2025-01-09 16:41:00 -0300

    [FG-29]-remove: Remoção de Scan
    ---- Modificações:
    
    --- Foi removido o @ComponentScan

--- src/main/java/br/edu/ifpb/pweb2/flashg/FlashGApplication.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/FlashGApplication.java
@@ -2,10 +2,8 @@ package br.edu.ifpb.pweb2.flashg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.edu.ifpb.pweb2.flashg") // Adicionado para o Spring encontrar os controllers
 public class FlashGApplication {
 
 	public static void main(String[] args) {
