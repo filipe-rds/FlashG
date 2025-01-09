4df3f26cd446724b106c90d84debf543b19ddd40
Gabriel Félix <gfedacs@hotmail.com>
2025-01-09 16:47:05 -0300

    [FG-29]-bug: Quebra de página
    ---- Modificações:
    
    Foi adicionado um if pra verificar se as listas listaDeSeguidos e listaDeSeguidores são nulas.

--- src/main/java/br/edu/ifpb/pweb2/flashg/service/FacadeService.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/service/FacadeService.java
@@ -73,9 +73,15 @@ public class FacadeService {
         }
     }
     
-    // Seguido 
-    public Follow isFollowed(Photographer Seguidor,Photographer Seguido){ 
+     // Seguido 
+     public Follow isFollowed(Photographer Seguidor,Photographer Seguido){ 
+
         List<Follow> listaDeSeguidos = Seguidor.getFollowing();
+
+        if (listaDeSeguidos == null) {
+            return null;
+        } 
+    
         for(Follow f : listaDeSeguidos){
             if(f.getFollowed().getId().equals(Seguido.getId())){
                 return f;
@@ -87,6 +93,11 @@ public class FacadeService {
     // Seguidor
     public Follow isFollower(Photographer Seguido,Photographer Seguidor){
         List<Follow> listaDeSeguidores = Seguido.getFollowers();
+
+        if (listaDeSeguidores == null) {
+            return null;
+        }
+        
         for(Follow f : listaDeSeguidores){
             if(f.getFollower().getId().equals(Seguidor.getId())){
                 return f;
@@ -94,7 +105,6 @@ public class FacadeService {
         }
         return null;
     }
-
     public void unfollow(Photographer p1,Photographer p2,Follow follow){
 
         List<Follow> listaDeSeguindo = p1.getFollowing();
