1bb0c79b2c9e869a1ab46abdbce8e785d5b23163
LucasJaud <lucasjaud19@gmail.com>
2025-03-11 13:13:17 -0300

    [FG-129]feat: criação da query para pega lista de Tags a partir de uma String.

--- src/main/java/br/edu/ifpb/pweb2/flashg/repository/TagRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/repository/TagRepository.java
@@ -2,10 +2,13 @@ package br.edu.ifpb.pweb2.flashg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifpb.pweb2.flashg.entity.Tag;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {   
 
-    Optional<Tag> findBytagname(String tagName);
+    Optional<Tag> findByTagName(String tagName);
+
+    List<Tag> findByTagNameContainingIgnoreCase(String tagName);
 }
