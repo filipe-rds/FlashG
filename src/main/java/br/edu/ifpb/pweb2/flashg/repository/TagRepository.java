f4b37d7e3cf0fc799b98b17ce2dc3c93faaef87f
Lucas Pedro <lucasjaud19@gmail.com>
2025-03-10 21:52:20 -0300

    [FG-129] feat: criação do service da tag e mudança no repositoria da tag

--- src/main/java/br/edu/ifpb/pweb2/flashg/repository/TagRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/flashg/repository/TagRepository.java
@@ -2,7 +2,10 @@ package br.edu.ifpb.pweb2.flashg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifpb.pweb2.flashg.entity.Tag;
 
+import java.util.Optional;
+
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {   
-    
+
+    Optional<Tag> findBytagname(String tagName);
 }
---
+++ src/main/java/br/edu/ifpb/pweb2/flashg/service/TagService.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.pweb2.flashg.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TagService {
+
+}
